dd570b55dc804888c0abc4c818d35c4a00b16b52
henrytwagner <htwags22@gmail.com>
2025-04-26 10:59:52 +0200

    Update contact list serializer to pull in linked profile info

--- contacts/api/serializers.py
+++ contacts/api/serializers.py
@@ -1,21 +1,37 @@
+# serializers.py
 from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from users.models import UserProfile
-from organizations.models import Organization, UserOrganization, ContactOrganization
 from contacts.models import Contact
 
 class ContactSerializer(serializers.ModelSerializer):
+    # pull username off the linked User
+    username = serializers.CharField(
+        source='linked_profile.username',
+        read_only=True,
+        default=''
+    )
+    # pull profile image URL off the related UserProfile
+    profile_image_url = serializers.CharField(
+        source='linked_profile.profile.profile_image_url',
+        read_only=True,
+        default=''
+    )
+
     class Meta:
         model = Contact
         fields = [
             'id',
             'user',
             'linked_profile',
+
+            'username',
+            'profile_image_url',
+
             'first_name',
             'last_name',
             'email',
             'phone',
+
             'created_at',
-            'updated_at'
+            'updated_at',
         ]
         read_only_fields = ['id', 'user', 'created_at', 'updated_at']
